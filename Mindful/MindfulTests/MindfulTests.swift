6229e7359d5efdb26a71abf87e8f53c7bb83c945
VladimirTM <90709606+VladimirTM@users.noreply.github.com>
2024-11-09 18:31:25 +0200

    Added the UI for the app

---
+++ Mindful/MindfulTests/MindfulTests.swift
@@ -0,0 +1,17 @@
+//
+//  MindfulTests.swift
+//  MindfulTests
+//
+//  Created by Vladimir Ghimpau on 08.11.2024.
+//
+
+import Testing
+@testable import Mindful
+
+struct MindfulTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
